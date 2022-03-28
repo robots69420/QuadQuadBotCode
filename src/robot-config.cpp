54577760888032a1079e0067451613a8915fa21d
robots69420 <94572590+robots69420@users.noreply.github.com>
2022-03-28 14:04:29 -0500

    Create robot-config.cpp

---
+++ src/robot-config.cpp
@@ -0,0 +1,30 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor LeftDriveSmart = motor(PORT1, ratio36_1, false);
+motor RightDriveSmart = motor(PORT2, ratio36_1, true);
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
+motor Arm = motor(PORT7, ratio36_1, false);
+motor LeftArm = motor(PORT3, ratio18_1, false);
+motor RightArm = motor(PORT8, ratio18_1, false);
+motor Claw = motor(PORT6, ratio18_1, false);
+
+// VEXcode generated functions
+
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
